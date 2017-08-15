5e19200c6d14e04bdb9623bfe070923267dd634f
Greg Dardis <dardis.greg@gmail.com>
2017-08-15 15:59:42 -0600

    added testing in main method, documentation

--- src/Karatsuba.java
+++ src/Karatsuba.java
@@ -1,9 +1,16 @@
 import java.math.BigInteger;
+import java.util.Scanner;
 
 /**
  * This class multiplies (often very large) integers using Karatsuba's algorithm.
+ * BigIntegers are utilized to prevent overflow, and allow HUGE numbers to be multiplied together almost instantly.
+ * 
+ * The program prompts the user for two strings containing only numbers,
+ * which are turned into BigIntegers, then their product is printed to the console.
  */
 public class Karatsuba {
+    
+    private static Scanner scanner = new Scanner(System.in);
     
     public BigInteger multiply(BigInteger x, BigInteger y) {
         // Once the problem is reduced down to single digit multiplication, simply multiply.
@@ -29,8 +36,25 @@ public class Karatsuba {
     
     public static void main(String[] args) {
         Karatsuba karatsuba = new Karatsuba();
-        BigInteger n1 = new BigInteger("3141592653589793238462643383279502884197169399375105820974944592");
-        BigInteger n2 = new BigInteger("2718281828459045235360287471352662497757247093699959574966967627");
-        System.out.println("Product: " + karatsuba.multiply(n1, n2));
+        System.out.println("This program multiplies two numbers together.\nThey can be huge numbers!\nFor example: ");
+        BigInteger n1 = new BigInteger("314462643383279502884197169399375105820974944592");
+        BigInteger n2 = new BigInteger("277757247093699959574966967627");
+        System.out.print(n1 + " * " + n2 + " = " + karatsuba.multiply(n1, n2) + "\n");
+        while (true) {
+            System.out.println("\nEnter two numbers to find their product (-1 to quit): ");
+            
+            System.out.print("Number 1: ");
+            String str1 = scanner.nextLine();
+            if (str1.equals("-1")) break;
+            BigInteger num1 = new BigInteger(str1);
+            
+            System.out.print("Number 2: ");
+            String str2 = scanner.nextLine();
+            if (str2.equals("-1")) break;
+            BigInteger num2 = new BigInteger(str2);
+            
+            System.out.println("Product: " + karatsuba.multiply(num1, num2));
+        }
+        System.out.println("Goodbye.");
     }
 }
